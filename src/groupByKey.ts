ce52a7c17e9e63b0d6b49ef97c15b93e1f258323
Viktor Pastushenko <pastushenko.vik@gmail.com>
2025-06-30 18:47:41 +0300

    add task solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,30 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey(items: object[], key: string): {} {
+  if (items.length === 0) {
+    return {};
+  }
+
+  const ITEM = items[0][key];
+
+  type ItemType = typeof ITEM;
+
+  const groups: GroupsMap<ItemType> = items.reduce(
+    (keyValues: GroupsMap<ItemType>, item: ItemType) => ({
+      ...keyValues,
+      [item[key]]: [],
+    }),
+    {},
+  );
+
+  for (const group of Object.keys(groups)) {
+    groups[group] = items.filter((item: ItemType) => {
+      return typeof item[key] === 'number'
+        ? item[key] === +group
+        : item[key] === group;
+    });
+  }
+
+  return groups;
 }
